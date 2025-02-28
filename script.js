4b4138cb36894dc18ea08aeacae7bc4cb90976d4
PHOOMPHAT <phomphat2011@gmail.com>
2025-02-28 15:38:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ document.addEventListener('contextmenu', function(e) {
   e.preventDefault();
 });
 
-document.addEventListener('mousedown', function(e) {
-  if (e.button === 0) {
-      e.preventDefault();
-  }
-});
-
 document.addEventListener('keydown', function(e) {
   if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
       e.preventDefault();
